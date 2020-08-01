627fe8fd556433007c86db7f344bf68616b29644
hrederik <32139140+hrederik@users.noreply.github.com>
2020-08-01 15:12:56 +0300

    Create GameState. Add speed control to Player

---
+++ Assets/Group1/Scripts/GameState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    [SerializeField] private EnemiesContainer _enemiesContainer;
+    [SerializeField] private UserInterface _userInterface;
+    [SerializeField] private Player _player;
+
+    private void OnEnable()
+    {
+        _enemiesContainer.EnemiesEnded += OnEnemiesEnded;
+    }
+
+    private void OnDisable()
+    {
+        _enemiesContainer.EnemiesEnded -= OnEnemiesEnded;
+    }
+
+    private void OnEnemiesEnded()
+    {
+        _userInterface.ShowGameOverPanel();
+        _player.Stop();
+    }
+}
--- Assets/Group1/Scripts/Player/Player.cs
+++ Assets/Group1/Scripts/Player/Player.cs
@@ -3,11 +3,13 @@
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    private float _defaultSpeed;
     private Transform _transform;
-    
+
     private void Start()
     {
         _transform = transform;
+        _defaultSpeed = _speed;
     }
 
     private void FixedUpdate()
@@ -17,6 +19,21 @@ public class Player : MonoBehaviour
         Vector2 direction = new Vector2(x, y);
 
         Move(direction);
+    }
+
+    public void SetSpeed(float targetSpeed)
+    {
+        _speed = targetSpeed;
+    }
+
+    public void Stop()
+    {
+        SetSpeed(0);
+    }
+
+    public void ResetSpeed()
+    {
+        _speed = _defaultSpeed;
     }
 
     private void Move(Vector2 direction)
