e081e28d9902d07c1a3e327751adfa2fc9680123
hrederik <32139140+hrederik@users.noreply.github.com>
2020-08-01 13:09:55 +0300

    Add PlayerTrigger

---
+++ Assets/Group1/Scripts/PlayerTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerTrigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _playerInterracted;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.TryGetComponent<Player>(out Player player))
+        {
+            _playerInterracted?.Invoke();
+        }
+    }
+}
